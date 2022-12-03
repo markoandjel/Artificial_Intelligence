a98ce1099e5b8501b6588a6d766e460bf3da18ae
markoandjel <60831775+markoandjel@users.noreply.github.com>
2022-12-03 23:08:37 +0100

    Kraj prve faze

--- Projekat/domineering.py
+++ Projekat/domineering.py
@@ -1,15 +1,57 @@
 import pygame
 class Domineering:
-    def __init__(self, stanje = [[]], x_na_potezu = True,screen=None,velicinaPolja=50,m=8,n=8):
+    def __init__(self, stanje = [[]], x_na_potezu = True,screen=None,velicinaPolja=50,m=8,n=8,VI=False): #konstruktor
         self.stanje = stanje
         self.x_na_potezu = x_na_potezu
         self.screen=screen
         self.velicinaPolja=velicinaPolja
         self.m=m #m-broj vrsta
         self.n=n #n-broj kolona
+        self.VI=VI
+    
+    def pocetni_parametri(self):
+        print("Unesite velicinu table u mxn")
+        self.m=int(input())
+        self.n=int(input())
+        nema_prvi_igrac = True
+        nema_prvi_simbol = True
+        while nema_prvi_igrac:
+            covek_prvi = input("Čovek igra prvi?(D/N)\n")
+            if covek_prvi == "D":
+                nema_prvi_igrac = False
+                covek_prvi = True
+            elif covek_prvi == "N":
+                nema_prvi_igrac = False
+                covek_prvi = False
+            elif covek_prvi == "d":
+                nema_prvi_igrac = False
+                covek_prvi = True
+            elif covek_prvi == "n":
+                nema_prvi_igrac = False
+                covek_prvi = False
 
-    def crtaj_tablu(self):
+        while nema_prvi_simbol:
+            x_prvi = input("X igra prvi?(D/N)\n")
+            if x_prvi == "D":
+                nema_prvi_simbol = False
+                x_prvi = True 
+            elif x_prvi == "N":
+                nema_prvi_simbol = False
+                x_prvi = False 
+            elif x_prvi == "d":
+                nema_prvi_simbol = False
+                x_prvi = True   
+            elif x_prvi == "n":
+                nema_prvi_simbol = False
+                x_prvi = False 
+
+        self.VI=not covek_prvi
+        self.x_na_potezu=x_prvi
+
+
+    def crtaj_tablu(self): #crtanje prazne tabele, sa brojevima i slovima polja
         self.velicinaPolja=int(self.screen.get_size()[0]/(self.n+2 if(self.n>self.m) else self.m+2))
+        #boardscore=
         tabla=pygame.Surface(((self.n+1)*self.velicinaPolja,(self.m+1)*self.velicinaPolja))
         tabla.fill((0,0,0))
         p=True
@@ -28,6 +70,19 @@ class Domineering:
         pygame.draw.rect(tabla,backgroundColor,(0,0,self.velicinaPolja,self.velicinaPolja*self.m))
         pygame.draw.rect(tabla,backgroundColor,(0,self.velicinaPolja*self.m,self.velicinaPolja*(self.n+1),self.velicinaPolja))
         font=pygame.font.SysFont(None, int(self.velicinaPolja))
+
+        if self.kraj_igre():
+            igrac='O' if self.x_na_potezu else 'X'
+            img = font.render(f'Pobednik je igrac {igrac}', True, (0,0,0))
+        else:
+            igrac='X' if self.x_na_potezu else 'O'
+            img = font.render(f'Na potezu je igrac {igrac}', True, (0,0,0))
+
+        
+        rect = img.get_rect()
+        pygame.draw.rect(img, (0,0,0), rect,-1)
+        self.screen.blit(img,(int(self.velicinaPolja),int(self.velicinaPolja/4)))
+
         pomeraj=self.velicinaPolja/4
         
         for i in range (self.n): #crtanje slova
@@ -43,29 +98,29 @@ class Domineering:
             tabla.blit(img,(pomeraj*2,self.velicinaPolja*(i-1)+pomeraj))
         
         self.screen.blit(tabla,(0,self.velicinaPolja))
-    def pocetno_stanje(self):
+    def pocetno_stanje(self): #postavlja matricu stanja na None
         self.stanje.clear()
         self.stanje = [[None for i in range(self.n)] for j in range(self.m)]
 
-    def prikaz_stanja(self):
+    def prikaz_stanja(self): #prikazuje trenutno stanje table
         for i in range(self.m-1,-1,-1):
             for j in range(self.n):
                 if self.stanje[i][j]==0:
                     pygame.draw.rect(self.screen,(47, 44, 255),((j+1)*self.velicinaPolja,i*self.velicinaPolja+self.velicinaPolja,self.velicinaPolja,self.velicinaPolja))
                 elif self.stanje[i][j]==1:
                     pygame.draw.rect(self.screen,(222, 56, 44),((j+1)*self.velicinaPolja,i*self.velicinaPolja+self.velicinaPolja,self.velicinaPolja,self.velicinaPolja))
-    def proveri_klik(self,pos):
+    def proveri_klik(self,pos): #proverava da li je korisnik kliknuo na validnu poziciju na tabli
         (x,y)=pos
         x=x-self.velicinaPolja
         y=y-self.velicinaPolja
         if 0<x<self.velicinaPolja*(self.n) and 0<y<self.velicinaPolja*(self.m):
             self.unesi_potez((int(y/self.velicinaPolja),int(x/self.velicinaPolja)))
 
-    def unesi_potez(self,pos):
+    def unesi_potez(self,pos): #unos poteza kao i provera njegove validnosti
         if (self.potez_validan(pos)):
             if (self.x_na_potezu):
                 self.stanje[pos[0]][pos[1]]=1
-                self.stanje[pos[0]+1][pos[1]]=1
+                self.stanje[pos[0]-1][pos[1]]=1
             elif (not self.x_na_potezu):
                 self.stanje[pos[0]][pos[1]]=0
                 self.stanje[pos[0]][pos[1]+1]=0
@@ -73,21 +128,21 @@ class Domineering:
         else:
             print('Uneli ste nevalidan potez')
 
-    def kraj_igre(self):
+    def kraj_igre(self): #provera da li je igra došla do kraja
         for i in range(self.m):
             for j in range(self.n):
                 if self.potez_validan((i,j)):
                     return False
         return True
-    def potez_validan(self, pos):
+    def potez_validan(self, pos): #provera validnosti poteza
         if self.x_na_potezu:
-            if  not (len(self.stanje) - 1 > pos[0] >= 0):
+            if  not (len(self.stanje) > pos[0] >= 1):
                 return False
             if not (len((self.stanje)[0]) > pos[1] >= 0):
                 return False
             if not self.stanje[pos[0]][pos[1]] == None:
                 return False
-            if not self.stanje[pos[0]+1][pos[1]] == None:
+            if not self.stanje[pos[0]-1][pos[1]] == None:
                 return False
         else:
             if  not (len(self.stanje) > pos[0] >= 0):
--- Projekat/main.py
+++ Projekat/main.py
@@ -5,20 +5,20 @@ pygame.init()
 clock=pygame.time.Clock()
 # Postavi ekran
 screen = pygame.display.set_mode([800, 800])
-dominacija = Domineering(screen=screen,m=7,n=7)
+
+
+dominacija = Domineering(screen=screen)
+
+dominacija.pocetni_parametri()
 
 dominacija.pocetno_stanje()
-dominacija.unesi_potez((2,2))
-dominacija.unesi_potez((5,5))
-
-dominacija.unesi_potez((1,5))
-
-dominacija.unesi_potez((0,3))
+#dominacija.unesi_potez((2,2))
+#dominacija.unesi_potez((5,5))
+#dominacija.unesi_potez((1,5))
+#dominacija.unesi_potez((0,3))
 running = True
   
 while running:
-    #igrac='X' if dominacija.x_na_potezu else 'O'
-    #print(f'Na potezu je igrac {igrac}')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
